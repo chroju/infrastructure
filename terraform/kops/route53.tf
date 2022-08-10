a3f32ac8bc8a76eef195d955c5ea7008b2c466a5
chroju <chroju@users.noreply.github.com>
2022-08-10 23:22:36 +0900

    Terraformを一度リセット

--- terraform/kops/route53.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_route53_zone" "kops" {
-  name = "kops.chroju.dev"
-}
--- terraform/kops/s3.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_bucket" "kops_config" {
-  bucket = "chroju-kops-config"
-  versioning {
-    enabled = true
-  }
-}
