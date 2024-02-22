154f87e50d817ba09cafc3e5278746a5b274c047
chroju <chroju@users.noreply.github.com>
2024-02-22 10:04:40 +0900

    add: AWS Backup

---
+++ terraform/k3s/backup.tf
@@ -0,0 +1,38 @@
+data "aws_kms_alias" "backup" {
+  name = "alias/aws/backup"
+}
+
+data "aws_iam_role" "backup_default_role" {
+  name = "AWSBackupDefaultServiceRole"
+}
+
+resource "aws_backup_vault" "default" {
+  name        = "Default"
+  kms_key_arn = data.aws_kms_alias.backup.target_key_arn
+}
+
+resource "aws_backup_plan" "daily_ec2_backup" {
+  name = "DailyEC2Backup"
+
+  rule {
+    rule_name         = "DailyEC2Backup"
+    target_vault_name = aws_backup_vault.default.name
+
+    schedule = "cron(0 5 ? * * *)"
+
+    lifecycle {
+      delete_after = 14
+    }
+  }
+}
+
+resource "aws_backup_selection" "k3s" {
+  name = "k3s"
+
+  iam_role_arn = data.aws_iam_role.backup_default_role.arn
+  plan_id      = aws_backup_plan.daily_ec2_backup.id
+
+  resources = [
+    aws_ebs_volume.k3s_data.arn,
+  ]
+}
